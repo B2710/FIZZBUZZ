f76230197a5a38a1bcdc92a2bd340213ab550f0b
Error734862947 <93121855+B2710@users.noreply.github.com>
2022-04-13 12:43:01 +0000

    unnecessary code gone

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 for x in range(101):
   if x == 0:
-      continue
-  if x % 3 == 0 and x % 5 == 0:
-   if True:
-    print("FIZZBUZZ")
-    continue  
-  if x % 3 == 0:
-   if True:
-    print("FIZZ")
     continue
-  if x % 5 == 0:
-   if True:
+  elif x % 3 == 0 and x % 5 == 0:
+    print("FIZZBUZZ")
+  elif x % 3 == 0:
+    print("FIZZ")
+  elif x % 5 == 0:
     print("BUZZ")
     if x == 100:
      print("You have reached 100")
-    else: 
-     continue
   else: 
    print(x)
